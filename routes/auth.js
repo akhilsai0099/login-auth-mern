792541f1ff0b9d1cf08f4e70e1dd6fb0530029b2
akhilsai0099 <akhilsai0099@gmail.com>
2023-04-29 13:31:39 +0530

    changed the method action to put instead of post for reset password

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const { register, login, forgotpassword, resetpassword } = require('../controlle
 router.route('/login').post(login)
 router.route('/register').post(register)
 router.route('/forgotpassword').post(forgotpassword)
-router.route('/resetpassword:resetToken').post(resetpassword)
+router.route('/resetpassword:resetToken').put(resetpassword)
 
 module.exports = router;
 
